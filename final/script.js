b2ae9394e904ae2f2cf2eb90d46a53cdb6b9cc8b
jordan <sorgel_jordan_leigh@student.smc.edu>
2020-06-09 14:09:07 -0700

    Changed JS from inline on open button

--- final/script.js
+++ final/script.js
@@ -1,15 +1,9 @@
 
 
-// function openNav() {
-//   document.getElementById("side_nav").style.width = "250px";
-//   document.getElementById("main").style.marginLeft = "250px";
-// }
+
+document.getElementById("openNav").onclick = function () { openNav() };
 
 
-// function closeNav() {
-//   document.getElementById("side_nav").style.width = "0";
-//   document.getElementById("main").style.marginLeft = "0";
-// }
 
 
 function openNav() {
